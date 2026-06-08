add19cb53b6f190512c2585058e823ca93ca8618
mohitrokaya69 <mohitrokaya69@gmail.com>
2026-06-09 00:33:27 +0545

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.java
@@ -0,0 +1,53 @@
+class Solution {
+    Queue<int[]> queue = new LinkedList<>();
+    int[] ij = new int[2];
+    public int orangesRotting(int[][] grid) {
+        for(int k=0;k<grid.length;k++){
+            for(int l=0;l<grid[0].length;l++){
+                if(grid[k][l]==2){
+                    queue.offer(new int[]{k,l});
+                }
+            }
+        }
+        int count=-1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            count++;
+            for(int r=0;r<size;r++){
+                ij = queue.poll();
+                int i=ij[0];
+                int j=ij[1];
+
+                grid[i][j]=2;
+
+                if(i-1>=0&&grid[i-1][j]==1){
+                    grid[i-1][j]=2;
+                    queue.offer(new int[]{i-1,j});
+                }
+                if(i+1<grid.length&&grid[i+1][j]==1){
+                    grid[i+1][j]=2;
+                    queue.offer(new int[]{i+1,j});
+                }
+                if(j-1>=0&&grid[i][j-1]==1){
+                    queue.offer(new int[]{i,j-1});
+                    grid[i][j-1]=2;
+                }
+                if(j+1<grid[0].length&&grid[i][j+1]==1){
+                    queue.offer(new int[]{i,j+1});
+                    grid[i][j+1]=2;
+                }
+            }
+        }
+        for(int k=0;k<grid.length;k++){
+            for(int l=0;l<grid[0].length;l++){
+                if(grid[k][l]==1){
+                    return -1;
+                }
+            }
+        }
+        if(count==-1){
+            count=0;
+        }
+        return count;
+    }
+}
